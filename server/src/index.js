56137b09dfb4a7f9c8bdf9f3d13d68cefa95b330
Oleksandr Iefymchuk <oleksandr_iefymchuk@epam.com>
2024-04-27 21:30:09 +0300

    add vercel.json

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ import { apiEndpoints } from './constants.js';
 import healthCheckRoute from './routes/healthCheck.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(apiEndpoints.HEALTH_CHECK, healthCheckRoute);
 
