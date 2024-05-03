0d6a3ca200ea8c2961b478700adf74f31c098a82
Oleksandr Iefymchuk <oleksandr_iefymchuk@epam.com>
2024-05-03 22:42:44 +0300

    fix PORT

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,7 +9,7 @@ import productsRouter from './routes/products.js';
 import reviewsRouter from './routes/reviews.js';
 
 export const app: express.Application = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
