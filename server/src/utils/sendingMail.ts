4ef28b5721ae0d5d137e3170d422410438bea34f
Oleksandr Iefymchuk <oleksandr_iefymchuk@epam.com>
2024-05-15 17:48:21 +0300

    fix sendingMail

--- server/src/utils/sendingMail.ts
+++ server/src/utils/sendingMail.ts
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer';
 const sendingMail = async (to: string, link: string): Promise<void> => {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
       user: 'bizmailer24@gmail.com',
       pass: 'rimzqfyxutxdpsdn'
